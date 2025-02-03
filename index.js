563c5479ab154178d2f3660aaaa063ba3490f0fb
Waleed Georgy <49490582+waleedGeorgy@users.noreply.github.com>
2025-02-03 21:33:25 +0300

    Version 2.3 - Minor visual updates

--- index.js
+++ index.js
@@ -80,5 +80,5 @@ app.post("/delete_list/:id", async(req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server running on port http://localhost:${port}`);
 });
